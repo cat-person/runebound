3f86308f8fd375477c6289bb55a4f5a0ae3d277d
Aleksei Klyshnniov <alexey.klyshnikov@gmail.com>
2024-02-29 01:21:24 +0400

    Removed unused stuff

--- fit.go
+++ fit.go
@@ -2,16 +2,13 @@ package main
 
 type FitName string
 
-// Racial
-const ()
-
 const (
-	Mountain      FitName = "Mountain"      // +1 str
-	Stoic         FitName = "Stoic"         // +1 wlp
-	SpiritSpeaker FitName = "SpiritSpeaker" // +1 wis
-	Scholar       FitName = "Scholar"       // +1 int
-	SilverTongue  FitName = "SilverTongue"  // +1 cha
-	IronGuts      FitName = "IronGuts"      // +1 vit
+	Mountain     FitName = "Mountain"     // +1 str
+	Stoic        FitName = "Stoic"        // +1 wlp
+	SpiritBound  FitName = "SpiritBound"  // +1 wis
+	Scholar      FitName = "Scholar"      // +1 int
+	SilverTongue FitName = "SilverTongue" // +1 cha
+	IronGuts     FitName = "IronGuts"     // +1 vit
 
 	Bully      FitName = "Bully"      // + 2 str - 1 int
 	Prude      FitName = "Prude"      // + 2 wlp - 1 cha
@@ -22,19 +19,19 @@ const (
 )
 
 var Fits = map[FitName]Fit{
-	Mountain:      Fit{map[StatName]int{Strength: 1}},
-	Stoic:         Fit{map[StatName]int{Willpower: 1}},
-	SpiritSpeaker: Fit{map[StatName]int{Wisdom: 1}},
-	Scholar:       Fit{map[StatName]int{Intelligence: 1}},
-	SilverTongue:  Fit{map[StatName]int{Charisma: 1}},
-	IronGuts:      Fit{map[StatName]int{Vitality: 1}},
+	Mountain:     {map[StatName]int{Strength: 1}},
+	Stoic:        {map[StatName]int{Willpower: 1}},
+	SpiritBound:  {map[StatName]int{Wisdom: 1}},
+	Scholar:      {map[StatName]int{Intelligence: 1}},
+	SilverTongue: {map[StatName]int{Charisma: 1}},
+	IronGuts:     {map[StatName]int{Vitality: 1}},
 
-	Bully:      Fit{map[StatName]int{Strength: 2, Intelligence: -1}},
-	Prude:      Fit{map[StatName]int{Willpower: 2, Intelligence: -1}},
-	Elder:      Fit{map[StatName]int{Wisdom: 2, Intelligence: -1}},
-	Bookworm:   Fit{map[StatName]int{Intelligence: 2, Strength: -1}},
-	Sensualist: Fit{map[StatName]int{Charisma: 2, Willpower: -1}},
-	Bear:       Fit{map[StatName]int{Vitality: 2, Wisdom: -1}},
+	Bully:      {map[StatName]int{Strength: 2, Intelligence: -1}},
+	Prude:      {map[StatName]int{Willpower: 2, Intelligence: -1}},
+	Elder:      {map[StatName]int{Wisdom: 2, Intelligence: -1}},
+	Bookworm:   {map[StatName]int{Intelligence: 2, Strength: -1}},
+	Sensualist: {map[StatName]int{Charisma: 2, Willpower: -1}},
+	Bear:       {map[StatName]int{Vitality: 2, Wisdom: -1}},
 }
 
 type Fit struct {
